62f137ac16da831e6fc4d8e20c53d531193863b2
Arqam699 <arqammughal699@gmail.com>
2025-08-28 20:56:31 +0500

    change number from + tu -

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
-console.log("hello world");
+let number= 2
+let negnumber1 = -number
+console.log(negnumber1);
 
-console.log("hello world2");
-console.log("hello world3");
+let number2= -2
+let negnumber2 = - number2
+console.log(negnumber2);
+
