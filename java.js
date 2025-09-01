8543dc155922deabc371f8cb4020f7b32f286225
Arqam699 <arqammughal699@gmail.com>
2025-09-01 20:08:31 +0500

    strings in js

--- java.js
+++ java.js
@@ -7,3 +7,30 @@ let negnumber2 = - number2
 console.log(negnumber2);
 
 
+// strings methods
+let name = "    Arqam    "
+let age = 20;
+let anothername = "Arqam"
+
+console.log(anothername.indexOf("q"));
+
+console.log(anothername.charAt(2));
+
+
+console.log(anothername.substring(0,3));
+
+console.log(name);
+
+console.log(name.trim());
+ 
+console.log(anothername.replace("Arqam","Arqam Mghal".toUpperCase()));
+
+console.log(anothername.split("a"));
+
+
+
+
+
+
+
+
