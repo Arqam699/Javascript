d5a68354e241d8d4c82afc253fa40a21e3c564e4
Arqam699 <arqammughal699@gmail.com>
2025-09-01 20:28:43 +0500

    number mehods

---
+++ numbermethods.js
@@ -0,0 +1,27 @@
+
+//node numbermethods.js
+
+//number methods
+
+ let score =100;
+ let bonus =20
+ 
+ console.log(typeof bonus);
+ 
+ 
+ console.log(score.toString());
+
+ console.log(typeof score);
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
