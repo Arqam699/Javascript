a7938578853e17dafa08546a364e6a2da53844b1
Arqam699 <arqammughal699@gmail.com>
2025-09-01 20:29:21 +0500

    strings mehods in js

---
+++ stringsmethods.js
@@ -0,0 +1,21 @@
+
+// node stringsmethods.js
+
+// strings methods
+let name = "    Arqam    "
+let anothername = "Arqam"
+
+console.log(anothername.indexOf("q"));
+
+console.log(anothername.charAt(2));
+
+
+console.log(anothername.substring(0,3));
+
+console.log(name);
+
+console.log(name.trim());
+ 
+console.log(anothername.replace("Arqam","Arqam Mghal".toUpperCase()));
+
+console.log(anothername.split("a"));
