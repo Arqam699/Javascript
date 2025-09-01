1f1b16d546f08e204fabf1f13012f0690b48fa5f
Arqam699 <arqammughal699@gmail.com>
2025-09-01 20:46:40 +0500

    maths methods in js

---
+++ maths.js
@@ -0,0 +1,25 @@
+//node maths.js
+
+
+console.log(Math.abs(-10));
+
+console.log(Math.sqrt(81));
+
+console.log(Math.ceil(5.1));
+
+console.log(Math.floor(5.2));
+
+console.log(Math.round(20.9));
+
+console.log(Math.min(1,5,3,8,9));
+
+console.log(Math.max(1,5,3,8,9));
+
+console.log(Math.random()); //give random number between 0 and 1
+
+console.log(Math.random()*10); //give random number between 0 and 10
+
+console.log(Math.round(Math.random()*100)); //give random number between 0 and 100 without decimal also gives 0
+
+console.log(Math.round(Math.random()*10));  //giver random number between 0 and 10 without decimal also give 0
+
