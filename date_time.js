ff7815742300db4298090697bb12b8d5463c7717
Arqam699 <arqammughal699@gmail.com>
2025-09-01 22:11:40 +0500

    date time methods in js

---
+++ date_time.js
@@ -0,0 +1,28 @@
+// node date_time.js
+
+
+let date = new Date()
+
+console.log(date);
+
+console.log(date.toLocaleString()); // it makes date and time more read able
+
+console.log(date.toDateString()); // it makes date and time more read able
+
+console.log(date.toTimeString()); // it makes date and time more read able
+
+let createdate = new Date(2003,8,25); // year , month , date
+
+console.log(createdate.toDateString()); 
+
+let createdate2 = new Date(2003,8,25,12,0,0);  // year , month , date time
+
+console.log(createdate2.toLocaleString());
+
+
+
+
+
+
+ 
+
